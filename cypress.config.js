dfbede4414e8e8333986300e334f8df5507892a5
hemaghanta08 <ghanta.hema08@gmail.com>
2024-06-26 00:20:52 +0530

    Form Submission Testcase with Report

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      // Implement any additional node event listeners here
+    },
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    ignoreVideos: false
+    
+  },
+  screenshotOnRunFailure: true,
+  video:true
+ 
+});
