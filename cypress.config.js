17923f5e251aedd5af7f5de90e842b83d10816a4
hemaghanta08 <ghanta.hema08@gmail.com>
2024-06-26 00:44:51 +0530

    Added ViewPort

--- cypress.config.js
+++ cypress.config.js
@@ -3,24 +3,24 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
       // Implement any additional node event listeners here
     },
   },
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
-    reportDir: 'cypress/reports',
+    reportDir: "cypress/reports",
     overwrite: false,
     html: true,
     json: true,
-    ignoreVideos: false
-    
+    ignoreVideos: false,
   },
   screenshotOnRunFailure: true,
-  video:true,
+  video: true,
   retries: {
     runMode: 2,
-    openMode: 0
-  }
- 
+    openMode: 0,
+  },
+  viewportWidth: 1440,
+  viewportHeight: 900,
 });
--- cypress/e2e/form_submission_test.cy.js
+++ cypress/e2e/form_submission_test.cy.js
@@ -1,18 +1,21 @@
-describe('Form Submission Test',()=>{
-    it('Submit the details in the form',()=>{
-//Navigate to the URL by using cy.visit
-cy.visit("https://docs.google.com/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform")
-//In the form, enter your name in the “Your Name” field.
-cy.get('.whsOnd').type('Nainisha Pandu')
-//Select the “18 and above” radio button.
-cy.get('#i12.Od2TWd').click()
-//Select “Yes” from the “Do you exercise every week?” dropdown.
-cy.get('div[class="e2CuFe eU809d"]').click()
-cy.get('div[role="option"]').contains("Yes").click()
-//Submit the form.
-cy.get('div[role="button"]').contains('Submit').click()
-//Verify that the form submission is successful (look for a confirmation message or achange in the URL).
-cy.url().should('include','formResponse')
-cy.contains('our response has been recorded.').should('be.visible')
-})
-})
+describe("Form Submission Test", () => {
+  it("Submit the details in the form", () => {
+    //cy.viewPort(1440,900)
+    //Navigate to the URL by using cy.visit
+    cy.visit(
+      "https://docs.google.com/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform"
+    );
+    //In the form, enter your name in the “Your Name” field.
+    cy.get(".whsOnd").type("Nainisha Pandu");
+    //Select the “18 and above” radio button.
+    cy.get("#i12.Od2TWd").click();
+    //Select “Yes” from the “Do you exercise every week?” dropdown.
+    cy.get('div[class="e2CuFe eU809d"]').click();
+    cy.get('div[role="option"]').contains("Yes").click();
+    //Submit the form.
+    cy.get('div[role="button"]').contains("Submit").click();
+    //Verify that the form submission is successful (look for a confirmation message or achange in the URL).
+    cy.url().should("include", "formResponse");
+    cy.contains("our response has been recorded.").should("be.visible");
+  });
+});
